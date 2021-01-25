5be5331f4bdc0f3e73490157bff5ddacd3d97a06
Khan <sebastian.ochoa1@udea.edu.co>
2021-01-25 14:04:20 -0500

    TestUpdate

--- src/test/java/com/arquisoft/cine/DataProviderClass.java
+++ src/test/java/com/arquisoft/cine/DataProviderClass.java
@@ -22,32 +22,31 @@ public class DataProviderClass {
             {"incorrecto","00000000"},
             {"0000000","incorrecto"},
             {"False1234","False1234"},
-            {"","0000"}
+            {"ADMIN","ADMIN"}
         };
     }
     
     @DataProvider(name = "RegistroNuevoData")
     public Object[][] getRegistroNuevoData() {
         return new Object[][]{
-            {"Alfa","apellido","0101"+random1,"Alfa."+random1+"@gmail.com","1010Alfa","Calle 60 Medellin","3001115566"},
-            {"Beta","apellido","0202"+random2,"Beta."+random2+"@outlook.com.co","2020Beta","Calle 80 Medellin","3002225566"},
-            {"Charlie","apellido","0303"+random3,"Charlie."+random3+"@microsoft.com","3030Charlie","Calle 100 Medellin","3003335566"}
+            {"Alfa","apellido","0101"+random1,"Alfa."+random1+"@gmail.com","1010Alfa","Citizenship card","Calle 60 Medellin","3001115566","05031990"},
+            {"Beta","apellido","0202"+random2,"Beta."+random2+"@outlook.com.co","2020Beta","Identity card","Calle 80 Medellin","3002225566","28091970"},
+            {"Charlie","apellido","0303"+random3,"Charlie."+random3+"@microsoft.com","3030Charlie","Passport","Calle 100 Medellin","3003335566","31122000"}
         };
     }
 
     @DataProvider(name = "RegistroDatosFaltantesData")
     public Object[][] RegistroDatosFaltantesData() {
         return new Object[][]{
-            {"","apellido","0102"+random1,"Alfa."+random1+"@gmail.com.es","1010Alfa","Calle 60 Medellin","3001115566"},
-            {"Beta","","0203"+random2,"Beta."+random2+"@outlook.com.co.es","2020Beta","Calle 80 Medellin","3002225566"},
-            {"Charlie","apellido","","Charlie."+random3+"@microsoft.com.es","3030Charlie","Calle 100 Medellin","3003335566"}
-            {"Alfa","apellido","0103"+random1,"","1010Alfa","Calle 60 Medellin","3001115566"},
-            {"Beta","apellido","0203"+random2,"Beta."+random2+"@outlook.com.co.es","","Calle 80 Medellin","3002225566"},
-            {"Charlie","apellido","0304"+random3,"Charlie."+random3+"@microsoft.com.es","3030Charlie","","3003335566"}
-            {"Alfa","apellido","0102"+random1,"Alfa."+random1+"@gmail.com.es","1010Alfa","Calle 60 Medellin",""},
-            {"Beta","apellido","0203"+random2,"Beta."+random2+"@outlook.com.co.es","2020Beta","Calle 80 Medellin","3002225566"},
-            {"Charlie","apellido","0304"+random3,"Charlie."+random3+"@microsoft.com.es","3030Charlie","Calle 100 Medellin",""}
-
+            {"","apellido","0102"+random1,"Alfa."+random1+"@gmail.com.es","1010Alfa","Citizenship card","Calle 60 Medellin","3001115566","05031990"},
+            {"Beta","","0203"+random2,"Beta."+random2+"@outlook.com.co.es","2020Beta","Identity card","Calle 80 Medellin","3002225566","28091970"},
+            {"Charlie","apellido","","Charlie."+random3+"@microsoft.com.es","3030Charlie","Passport","Calle 100 Medellin","3003335566","31122000"},
+            {"Alfa","apellido","0103"+random1,"","1010Alfa","Citizenship card","Calle 60 Medellin","3001115566","05031990"},
+            {"Beta","apellido","0203"+random2,"Beta."+random2+"@outlook.com.co.es","","Identity card","Calle 80 Medellin","3002225566","28091970"},
+            {"Charlie","apellido","0304"+random3,"Charlie."+random3+"@microsoft.com.es","3030Charlie","Passport","","3003335566","31122000"},
+            {"Alfa","apellido","0102"+random1,"Alfa."+random1+"@gmail.com.es","1010Alfa","Citizenship card","Calle 60 Medellin","","05031990"},
+            {"Beta","apellido","0203"+random2,"Beta."+random2+"@outlook.com.co.es","2020Beta","Identity card","Calle 80 Medellin","3002225566",""},
+            {"Charlie","apellido","0304"+random3,"Charlie."+random3+"@microsoft.com.es","3030Charlie","","Calle 100 Medellin","3003335566","31122000"}
         };
     }
 
